53f2babade4e42bfafd1c3db9deacedc4837e182
Kim Heidorn <heidorkn@gmail.com>
2019-02-09 19:46:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   return array_new.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array_new = []
+  array.collect do |string|
+    array_new << string.reverse
+  end
+  return array_new.join(" ")
+end
